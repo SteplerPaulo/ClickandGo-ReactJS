f43087a975dbb951b270191ecaa92f94d5a67726
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-04 18:42:04 +0800

    Update Manufacturer.js

--- src/views/Admin/Manufaturers.js
+++ src/views/Admin/Manufaturers.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { React, Fragment, useState, useEffect } from 'react'
+import axios from 'axios';
+import Table from 'components/Table/Table.js'
 
-export default function Manufaturers() {
+export default function Products() {
+    const [url] = useState(`${process.env.REACT_APP_API_URL}manufacturers/all`);
+    const [items, setItems] = useState([]);
+    const [tableName] = useState('Manufacturers');
+    const headCells = [
+        { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+        { id: 'slug', numeric: false, disablePadding: false, label: 'Slug' },
+        { id: 'address', numeric: false, disablePadding: false, label: 'Address' }
+    ];
+
+    useEffect(() => {
+        const response = async () => {
+            const { data } = await axios(url);
+            setItems(data);
+        }
+        response();
+    }, [url]);
+
+
+
     return (
-        <div>
-            <h2>Manufaturers</h2>
-        </div>
+        <Fragment>
+            <Table tableName={tableName} rows={items} headCells={headCells} />
+        </Fragment>
     )
 }
