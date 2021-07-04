784d79d6d2b03e488ba5cf8f0ce9febfc3b98372
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-04 18:39:05 +0800

    Rename Catalogue components to Catalog

--- src/components/Catalog/Items.js
+++ src/components/Catalog/Items.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-
 
 import { makeStyles } from '@material-ui/core/styles';
 
