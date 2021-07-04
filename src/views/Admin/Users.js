d2c71fd1ffd8d6090ecdc4d24a984283d10e2e62
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-04 18:41:30 +0800

    Update Admin Users

--- src/views/Admin/Users.js
+++ src/views/Admin/Users.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { React, Fragment, useState, useEffect } from 'react'
+import axios from 'axios';
+import Table from 'components/Table/Table.js'
 
-export default function Users() {
+export default function Products() {
+    const [url] = useState(`${process.env.REACT_APP_API_URL}users/all`);
+    const [items, setItems] = useState([]);
+    const [tableName] = useState('Users');
+    const headCells = [
+        { id: 'username', numeric: false, disablePadding: true, label: 'Username' },
+        { id: 'last_name', numeric: false, disablePadding: false, label: 'Last Name' },
+        { id: 'first_name', numeric: false, disablePadding: false, label: 'First Name' },
+        { id: 'middle_name', numeric: false, disablePadding: false, label: 'Middle Name' },
+        { id: 'is_admin', numeric: false, disablePadding: false, label: 'Admin' },
+    ];
+
+    useEffect(() => {
+        const response = async () => {
+            const { data } = await axios(url);
+            setItems(data);
+        }
+        response();
+    }, [url]);
+
     return (
-        <div>
-            <h2>Users</h2>
-        </div>
+        <Fragment>
+            <Table tableName={tableName} rows={items} headCells={headCells} />
+        </Fragment>
     )
 }
