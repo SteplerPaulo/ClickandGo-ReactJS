822325db2f3d9d7cbc0474bd6c77118529e7de0a
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-04 18:41:46 +0800

    Update Admin News.js

--- src/views/Admin/News.js
+++ src/views/Admin/News.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { React, Fragment, useState, useEffect } from 'react'
+import axios from 'axios';
+import Table from 'components/Table/Table.js';
 
 export default function News() {
+    const [url] = useState(`${process.env.REACT_APP_API_URL}banners/all`);
+    const [items, setItems] = useState([]);
+    const headCells = [
+        { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+        { id: 'caption', numeric: false, disablePadding: false, label: 'Caption' },
+        { id: 'is_active', numeric: true,bollean:true, disablePadding: false, label: 'Status' },
+    ];
+
+    useEffect(() => {
+        const response = async () => {
+            const { data } = await axios(url);
+            setItems(data);
+        }
+        response();
+    }, [url]);
+
+
+
     return (
-        <div>
-            <h2>News</h2>
-        </div>
+        <Fragment>
+            <Table tableName="Banners" rows={items} headCells={headCells} />
+        </Fragment>
     )
 }
