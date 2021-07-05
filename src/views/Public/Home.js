74ad04aa3b22aef9d86dbdd9a58a3a08491a6819
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-05 15:59:06 +0800

    Update Home.js

--- src/views/Public/Home.js
+++ src/views/Public/Home.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from 'axios';
-
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-
-import ItemList from 'components/Catalogue/Items.js'
+import ItemList from 'components/Catalog/Items.js'
 import Carousel from 'components/Carousel/Carousel.js'
+import { LoadMore, NoResult, EndOfResult } from 'components/Search/Result.js'
+
 require('dotenv').config()
 
 export default function Home() {
     const [page, setPage] = useState(1)
-    const [items, setItems] = useState([]);
+    const [totalPages, setTotalPages] = useState(1)
+    const [items, setItems] = useState([])
 
     useEffect(() => {
         const response = async () => {
-            const { data } = await axios(`${process.env.REACT_APP_API_URL}products?page=${page}&size=12`);
+            const { data } = await axios(`${process.env.REACT_APP_API_URL}products?page=${page}`);
+            setTotalPages(data.totalPages)
             setItems((prevItems) =>
                 [...new Set([...prevItems, ...data.rows])]
             );
@@ -24,11 +24,10 @@ export default function Home() {
         response();
     }, [page]);
 
-    const loadMore = () =>{
+    const loadMore = () => {
         let nextPage = page + 1;
         setPage(nextPage)
     }
-
 
     return (
         <Fragment>
@@ -40,13 +39,10 @@ export default function Home() {
                         <ItemList key={item.id} item={item} />
                     ))}
                 </Grid>
-                <Box textAlign='center' p={3}>
-                    <Button onClick={loadMore} variant='contained'>
-                        Load More
-                    </Button>
-                </Box>
+                {(items.length && (totalPages !== page))? <LoadMore loadMore={loadMore} /> :''}
+                {(items.length && (totalPages === page))? <EndOfResult/>:''}
+                {(!items.length)? <NoResult /> :''}
             </Container>
         </Fragment>
     )
 }
-
