1662efaf7c7dadb8d6e44190b4a98aa4a23bfb21
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-05 15:56:46 +0800

    Create Auto Complete Component

---
+++ src/components/AutoComplete/AutoComplete.js
@@ -0,0 +1,72 @@
+import { React, Fragment, useState, useEffect } from 'react'
+import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+export default function AutoComplete() {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [url] = useState(`${process.env.REACT_APP_API_URL}products/all`);
+  const loading = open && options.length === 0;
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      const response  = await axios(url);
+      if (active) {
+       setOptions(response.data);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading,url]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      style={{ width: 500 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
+      options={options}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search"
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <Fragment>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
