fc9c6a8641935d2eada7c6d4ad84bf5522e62194
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-05 20:00:08 +0800

    Update Admin News.js - Deploy Enhance Table

--- src/views/Admin/News.js
+++ src/views/Admin/News.js
@@ -1,20 +1,53 @@
 import { React, Fragment, useState, useEffect } from 'react'
 import axios from 'axios';
-import Table from 'components/Table/Table.js';
+import FloatingButton from 'components/FloatingButton/FloatingButton.js';
+import SearchInput from 'components/Search/Input.js';
+import Table from 'components/Pagination/Table.js'
 
 export default function News() {
-    const [url] = useState(`${process.env.REACT_APP_API_URL}banners/all`);
-    const [items, setItems] = useState([]);
     const headCells = [
-        { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-        { id: 'caption', numeric: false, disablePadding: false, label: 'Caption' },
-        { id: 'is_active', numeric: true,bollean:true, disablePadding: false, label: 'Status' },
+        { id: 'id', numeric: false, disablePadding: false,hidden: true, label: 'ID' },
+        { id: 'name', numeric: false, disablePadding: false,hidden: false, label: 'Name' },
+        { id: 'caption', numeric: true, disablePadding: false,hidden: false, label: 'Caption' }
     ];
+    const [items, setItems] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [query, setQuery] = useState('');
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(5);
+    const [baseUrl] = useState(`${process.env.REACT_APP_API_URL}banners`)
+    const [url, setUrl] = useState(baseUrl + `?size=${size}`);
+
+
+    const handleChangeSearch = (event) => {
+        setQuery(event.target.value)
+    }
+
+    const handleSearch = (event) => {
+        if (event.key === 'Enter' || event.type === 'click') {
+            setUrl(baseUrl + `?query=${query}&page=1&size=${size}`);
+        }
+    }
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+        setUrl(baseUrl + `?query=${query}&page=${newPage + 1}&size=${size}`);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        const size = parseInt(event.target.value, 10)
+        setSize(size);
+        setUrl(baseUrl + `?query=${query}&page=1&size=${size}`);
+    };
 
     useEffect(() => {
         const response = async () => {
             const { data } = await axios(url);
-            setItems(data);
+            console.log(data)
+            setItems(data.rows);
+            setTotalItems(data.totalItems);
+            setTotalPages(data.totalPages)
         }
         response();
     }, [url]);
@@ -23,7 +56,23 @@ export default function News() {
 
     return (
         <Fragment>
-            <Table tableName="Banners" rows={items} headCells={headCells} />
+           <SearchInput
+                items={items}
+                handleSearch={handleSearch}
+                handleChangeSearch={handleChangeSearch}
+            />
+            <FloatingButton />
+            <Table
+                tableName="Products"
+                rows={items}
+                page={page}
+                headCells={headCells}
+                totalItems={totalItems}
+                totalPages={totalPages}
+                handleChangePage={handleChangePage}
+                rowsPerPage={size}
+                handleChangeRowsPerPage={handleChangeRowsPerPage}
+                />
         </Fragment>
     )
 }
