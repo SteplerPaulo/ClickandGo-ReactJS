fd73d2b32496baca69d3986a389835e9e0471633
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-09 12:52:30 +0800

    Update Pagination Table Component

--- src/components/Pagination/Table.js
+++ src/components/Pagination/Table.js
@@ -6,6 +6,9 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+
+import EditIcon from '@material-ui/icons/Edit';
 
 import EnhancedTableHead from 'components/Pagination/TableHead.js'
 
@@ -31,10 +34,15 @@ export default function EnhancedTable(props) {
                         align={(col.numeric ? 'right' : 'left')}
                         className={col.hidden ? classes.hide : ''}
                       >
-
                         {row[col.id]}
                       </TableCell>
                     ))}
+
+                    <TableCell align="right">
+                      <IconButton>
+                        <EditIcon color="secondary"  />
+                      </IconButton>
+                    </TableCell>
                   </TableRow>
                 )
               })
