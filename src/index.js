37bcc22a41066b995ad5517d5a61e270219ae781
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-13 21:43:48 +0800

    Update Index Js
    Web App Authorization

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Admin from "layouts/Admin/Admin.js";
-import Default from "layouts/Default/Default.js";
+import { Router, Route, Switch } from "react-router-dom";
+import Admin from "layouts/Admin/Admin";
+import Default from "layouts/Default/Default";
+import Login from "components/Authorization/Login";
+import Register from "components/Authorization/Register";
+import useToken from "components/Authorization/useToken";
 
 const hist = createBrowserHistory();
 
-ReactDOM.render(
-  <BrowserRouter history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/" component={Default} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+const App = () => {
+  const { token, setToken } = useToken();
+  if (!token) {
+    return (
+      <Router history={hist}>
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/"  >
+            <Login setToken={setToken} />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+
+  return (
+    <Router history={hist}>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/" component={Default} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
