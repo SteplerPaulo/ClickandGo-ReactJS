dcec690653395be9466a1643867009a95a865eb0
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-13 21:40:38 +0800

    Create Login Component

---
+++ src/components/Authorization/Login.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import Copyright from "components/Authorization/Copyright";
+
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login({ setToken }) {
+  const classes = useStyles();
+  const [errorUsername,setErrorUsername] = useState({ error: false, message: "" });
+  const [errorPassword,serErrorPassword] = useState({ error: false, message: "" });
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateForm = () => {
+    return username.length > 0 && password.length > 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    serErrorPassword({ error: false, message: "" });
+    setErrorUsername({ error: false, message: "" });
+
+    const { data } = await axios.post(`http://localhost:8081/login`, {
+      username,
+      password,
+    });
+    
+    if(data.error){
+      switch(data.field){
+        case 'password':serErrorPassword(data);break;
+        default:setErrorUsername(data);break;
+      }
+    }else{
+      setToken(data);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            helperText={errorUsername.message}
+            error={errorUsername.error}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            helperText={errorPassword.message}
+            error={errorPassword.error}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={!validateForm()}
+          >
+            Log In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
