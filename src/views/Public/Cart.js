afe4e270a3d8c9e0dd9d7675adbc51b3ed56f97f
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-13 21:47:28 +0800

    Update Cart.js - Using GraphQL (Prototype)

--- src/views/Public/Cart.js
+++ src/views/Public/Cart.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { React, Fragment } from 'react'
 
 export default function Cart() {
+    var author = 'andy';
+    var content = 'hope is a good thing';
+    var query = `mutation CreateMessage($input: MessageInput) {
+                createMessage(input: $input) {
+                    id
+                }
+             }`;
+
+    fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                input: {
+                    author,
+                    content,
+                }
+            }
+        })
+    }).then(r => r.json()).then(data => console.log('data returned:', data));
+
+
     return (
-        <div>
-            CART
-        </div>
+        <Fragment>
+            Cart
+        </Fragment>
     )
 }
